ffe13b1f0d3b4346403e89875054ca83fceca66d
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-01-31 20:17:38 +0100

    Remove console logs

--- server/photos/list/index.js
+++ server/photos/list/index.js
@@ -5,14 +5,10 @@ import photoToClientFormatter from '../photo-to-client-formatter';
 export default () => {
     return new Promise((resolve, reject) => {
         return db.getPhotos()
-            .then(photos => {
-                console.log(photoToClientFormatter);
-                console.log(photoToClientFormatter(photos));
-                return resolve({
-                    base,
-                    photos: photoToClientFormatter(photos)
-                });
-            })
+            .then(photos => resolve({
+                base,
+                photos: photoToClientFormatter(photos)
+            }))
             .catch(err => reject(err));
     });
 };
