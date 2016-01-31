362cc89e86d65234fddaeb3cfc82fe52018b0acc
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-01-31 10:57:47 +0100

    Add test for fetch-reducer-factory

--- client/scripts/reducers/fetch-reducer-factory.js
+++ client/scripts/reducers/fetch-reducer-factory.js
@@ -4,25 +4,24 @@ export default ({initialDataValue, actionTypes = {}}) => {
         data: initialDataValue
     };
     return function (state = initialState, action = {}) {
-        const objectKey = action.objectKey || 'data';
         switch (action.type) {
         case actionTypes.REQUEST:
             return Object.assign({}, state, {
                 isFetching: true,
                 error: undefined,
-                [objectKey]: initialDataValue
+                data: initialDataValue
             });
         case actionTypes.RECEIVE:
             return Object.assign({}, state, {
                 isFetching: false,
                 error: undefined,
-                [objectKey]: action.data
+                data: action.data
             });
         case actionTypes.FETCH_ERROR:
             return Object.assign({}, state, {
                 isFetching: false,
                 error: action.error,
-                [objectKey]: initialDataValue
+                data: initialDataValue
             });
         default:
             return state;
---
+++ client/scripts/reducers/fetch-reducer-factory_test.js
@@ -0,0 +1,51 @@
+import reducerFactory from './fetch-reducer-factory';
+import {expect} from 'chai';
+
+describe('fetch-reducer-factory', () => {
+    let actionTypes, reducer;
+
+    beforeEach(() => {
+        actionTypes = {
+            REQUEST: 'req',
+            RECEIVE: 'rec',
+            FETCH_ERROR: 'error'
+        };
+        reducer = reducerFactory({initialDataValue: [], actionTypes});
+    });
+
+    it('should return object for REQUEST', () => {
+        const action = {
+            type: actionTypes.REQUEST
+        };
+        const state = reducer(undefined, action);
+
+        expect(state.isFetching).to.equal(true);
+        expect(state.error).to.be.undefined;
+        expect(state.data).to.eql([]);
+    });
+
+    it('should return object for RECEIVE', () => {
+        const action = {
+            type: actionTypes.RECEIVE,
+            data: [{foo: 'bar'}]
+        };
+        const state = reducer(undefined, action);
+
+        expect(state.isFetching).to.equal(false);
+        expect(state.error).to.be.undefined;
+        expect(state.data).to.eql([{foo: 'bar'}]);
+    });
+
+    it('should return object for FETCH_ERROR', () => {
+        const action = {
+            type: actionTypes.FETCH_ERROR,
+            error: 'An error'
+        };
+        const state = reducer(undefined, action);
+
+        expect(state.isFetching).to.equal(false);
+        expect(state.error).to.equal('An error');
+        expect(state.data).to.eql([]);
+    });
+
+});
