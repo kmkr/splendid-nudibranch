962b4bd5cba42444fd7dce2c0c701ed4120934bf
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-02-13 17:19:09 +0100

    Add base for collage

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,37 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+import Collage from './collage';
 import ListPhotos from './photos/list-photos';
 import {fetchPhotos} from './photos/photo-actions';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: this.getPage()
+        };
+    }
+
     componentWillMount() {
         this.props.dispatch(fetchPhotos());
+
+        window.onhashchange = () => {
+            this.setState({
+                page: this.getPage()
+            });
+        };
+    }
+
+    getPage() {
+        return /photos/.test(window.location.hash) ? 'photos' : 'collage';
     }
 
     render() {
         return (
-            <ListPhotos photos={this.props.photos.data} />
+            this.state.page === 'collage' ?
+                <Collage /> :
+                <ListPhotos photos={this.props.photos.data} />
         );
     }
 }
---
+++ client/scripts/collage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Collage extends React.Component {
+    render() {
+        return (
+            <div>
+                Collage inc
+                <a href="/#/photos">All photos</a>
+            </div>
+        );
+    }
+}
+
+export default Collage;
