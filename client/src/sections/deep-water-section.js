1bdf524f1cb40d3c965123a094471c71e612e742
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-02-16 22:02:32 +0100

    Include missing files

---
+++ client/src/sections/deep-water-section.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import smoothScroll from 'smooth-scroll';
+
+import './deep-water-section.scss';
+
+function onClick(e) {
+    e.preventDefault();
+    smoothScroll.animateScroll('#app');
+}
+
+class DeepWaterSection extends Component {
+    render() {
+        return (
+            <div id="deep-water-section">
+                <a href="#" onClick={onClick}>Go to top</a>
+            </div>
+        );
+    }
+}
+
+function select() {
+    return {};
+}
+
+export default connect(select)(DeepWaterSection);
