a871c8608dc419b3ff5ec3ae0e0e9cb59e041166
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-02-19 22:36:27 +0100

    Include font for title

--- client/src/photos/photo.js
+++ client/src/photos/photo.js
@@ -1,6 +1,8 @@
 import React, {Component, PropTypes} from 'react';
 import {resizeTo} from '../../../common/constants';
 
+import './photo.scss';
+
 class Photo extends Component {
     constructor(props) {
         super(props);
@@ -16,13 +18,14 @@ class Photo extends Component {
     render() {
         const {photoSize, photo} = this.props;
         return (
-            <div className="col-xs-14 col-xl-12 col-xl-offset-1">
+            <div className="photo col-xs-14 col-xl-12 col-xl-offset-1">
                 <img
                     style={{opacity: this.state.loaded ? 1 : 0}}
                     onLoad={this.loaded.bind(this)}
                     src={photo[photoSize]} />
-                <p>{photo.title}</p>
-                <p>{photo.description}</p>
+                <p className="title">{photo.title}</p>
+                <hr />
+                <p className="description">{photo.description}</p>
             </div>
         );
     }
