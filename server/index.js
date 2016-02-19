071803e829ec938a28bc5f5a3d00aa6e17000086
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-02-19 20:09:33 +0100

    Throw on startup if required env keys are missing

--- server/index.js
+++ server/index.js
@@ -10,6 +10,16 @@ import robotsRouter from './robots';
 
 const indexHtml = `${__dirname}/index.html`;
 
+function verifyEnv() {
+    const missing = ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'SN_DB_URL', 'SN_S3_BASE', 'SN_S3_BUCKET_NAME']
+        .filter(key => !process.env[key]);
+    if (missing.length) {
+        throw new Error(`Missing required env key(s) ${missing.join(', ')}`);
+    }
+}
+
+verifyEnv();
+
 const app = express();
 app.use(compression());
 app.use(logger('combined'));
