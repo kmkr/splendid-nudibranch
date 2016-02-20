02b17149c76c04a03c352c9f3f72e3fed35abca6
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-02-20 13:36:28 +0100

    Remove horiz scrollbar for small screens

--- client/src/photos/photo.js
+++ client/src/photos/photo.js
@@ -18,7 +18,7 @@ class Photo extends Component {
     render() {
         const {photoSize, photo} = this.props;
         return (
-            <div className="photo col-xs-14 col-xl-12 col-xl-offset-1">
+            <div className="photo col-xs-14">
                 <img
                     style={{opacity: this.state.loaded ? 1 : 0}}
                     onLoad={this.loaded.bind(this)}
