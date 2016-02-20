128a4d5144535bc6662f0b644fe3a7fb49aab15a
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-02-20 14:21:44 +0100

    Shuffle display of photos

--- client/src/photos/photo-actions.js
+++ client/src/photos/photo-actions.js
@@ -2,12 +2,24 @@ import actionTypes from './photo-action-types';
 import fetchActionFactory from '../actions/fetch-action-factory';
 import photoDataConversion from './photo-data-conversion';
 
+function shuffle(o) {
+    let i, j, x;
+    for (i = o.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = o[i - 1];
+        o[i - 1] = o[j];
+        o[j] = x;
+    }
+
+    return o;
+}
+
 export function fetchPhotos() {
     return fetchActionFactory({
         actionTypes,
         url: '/photos',
         responseHandler: data => (
-            data.photos.map(photo => photoDataConversion(photo, data.base))
+            shuffle(data.photos.map(photo => photoDataConversion(photo, data.base)))
         )
     });
 }
