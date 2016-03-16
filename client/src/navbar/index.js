6f4d7b9fab44b0bae8b7b4d3fffc7fecbfb64935
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-03-16 22:03:53 +0100

    Include navbar files

---
+++ client/src/navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './navbar.scss';
+
+const Navbar = ({children}) => (
+    <nav>
+    Splendid Nudibranch
+    {children}
+    </nav>
+);
+
+export default Navbar;
