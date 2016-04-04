8e717a05c5b87266b6b9fca4d09224e35578c690
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-04-04 22:22:50 +0200

    Clear cache after change is done

--- server/photos/index.js
+++ server/photos/index.js
@@ -10,26 +10,29 @@ const router = express.Router();
 const upload = multer();
 
 router.post('/', upload.single('file'), (req, res) => {
-    cache.clear();
-
     photoUploadHandler(req.file)
-        .then(response => res.json(response))
+        .then(response => {
+            cache.clear();
+            return res.json(response);
+        })
         .catch(error => res.status(500).json({error}));
 });
 
 router.put('/:id', (req, res) => {
-    cache.clear();
-
     updatePhotoHandler(req.params.id, req.body)
-        .then(response => res.json(response))
+        .then(response => {
+            cache.clear();
+            return res.json(response);
+        })
         .catch(error => res.status(500).json({error}));
 });
 
 router.delete('/:id', (req, res) => {
-    cache.clear();
-
     deletePhotoHandler(req.params.id)
-        .then(response => res.json(response))
+        .then(response => {
+            cache.clear();
+            return res.json(response);
+        })
         .catch(error => res.status(500).json({error}));
 });
 
