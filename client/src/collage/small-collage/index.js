d75bc262de1577ea0cf77d610eb121468c60f1af
Kris-Mikael Krister <krismikael@protonmail.com>
2016-07-31 22:13:40 +0200

    Switch to bouncing arrow

--- client/src/collage/small-collage/index.js
+++ client/src/collage/small-collage/index.js
@@ -3,7 +3,7 @@ import Vivus from 'vivus';
 import smoothScroll from 'smooth-scroll';
 
 import './small-collage.scss';
-import './pulsating-arrow.scss';
+import './arrow.scss';
 
 class SmallCollage extends Component {
     componentDidMount() {
@@ -26,11 +26,7 @@ class SmallCollage extends Component {
                         data="/static/images/logo-square.svg"/>
                     <img className="title-image" src="/static/images/title.svg"/>
                 </div>
-                <svg className="arrows" onClick={this.scrollToPhotoSection}>
-                    <path className="a1" d="M0 0 L30 32 L60 0"></path>
-                    <path className="a2" d="M0 20 L30 52 L60 20"></path>
-                    <path className="a3" d="M0 40 L30 72 L60 40"></path>
-                </svg>
+                <div className="arrow bounce" onClick={this.scrollToPhotoSection}/>
             </div>
         );
     }
