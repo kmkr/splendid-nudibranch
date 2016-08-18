7234acc9e4bf330c7f511918787c3fca713bf217
Kris-Mikael Krister <krismikael@protonmail.com>
2016-08-18 13:14:33 +0200

    Add disclaimer in license

--- server/views/layouts/license.jsx
+++ server/views/layouts/license.jsx
@@ -14,14 +14,19 @@ const License = () => (
                                       ´                    \\\`
                                       |____________________|
 
-   The Splendid Nudibranch is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License
+   Hi, and welcome!
+
+   The entire source code for this site can be found on https://github.com/kmkr/splendid-nudibranch
+
+   Content found on The Splendid Nudibranch (photos and text) is licensed under a
+   Creative Commons Attribution-ShareAlike 4.0 International License. If you don't mind, please
+   contact me if you use any of this. You can use krismikael <at> protonmail <dot> com
    -->
 
     <div style="display: none">
         <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
             <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" />
         </a>
-        <br />
         <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">The Splendid Nudibranch</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.thesplendidnudibranch.pink" property="cc:attributionName" rel="cc:attributionURL">Kris Mikael Krister</a>
             is licensed under a
         <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
