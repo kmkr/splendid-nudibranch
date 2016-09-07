81d2978cd310d539396710c37f88e73df30f257a
Kris-Mikael Krister <krismikael@protonmail.com>
2016-09-07 20:55:50 +0200

    Remove test depending on evn variable

--- server/photos/s3/s3-wrapper_test.js
+++ server/photos/s3/s3-wrapper_test.js
@@ -2,10 +2,6 @@ import test from 'ava';
 
 import {generateParams} from './s3-wrapper';
 
-test('return defaults', t => {
-    t.is(generateParams().Bucket, 'splendid-nudibranch-dev');
-});
-
 test('should assign extras', t => {
     t.is(generateParams({foo: 'bar'}).foo, 'bar');
 });
