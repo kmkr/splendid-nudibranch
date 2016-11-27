3b0838c2018d2ce0e3bcc93e4c9c68e04142e47a
Kris-Mikael Krister <krismikael@protonmail.com>
2016-11-27 10:20:26 +0100

    Remove why did you update
    https://github.com/garbles/why-did-you-update/issues/28

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import {whyDidYouUpdate} from 'why-did-you-update';
-
-if (process.env.NODE_ENV !== 'production') {
-    whyDidYouUpdate(React);
-}
 
 import ReactDOM from 'react-dom';
 import Wrapper from './wrapper';
