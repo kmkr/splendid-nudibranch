2cd51cb0f97baf0b45e43d0103267553927f5c17
Kris-Mikael Krister <krismikael@protonmail.com>
2017-01-31 20:03:13 +0100

    Clean up og-tag code

--- server/views/layouts/og-tags.js
+++ server/views/layouts/og-tags.js
@@ -3,8 +3,36 @@ import {description} from '../../../common/constants';
 
 const name = 'The Splendid Nudibranch';
 
+function buildUrl({selectedPhotoKey, year, location}) {
+    let url = 'http://www.thesplendidnudibranch.pink';
+
+    if (selectedPhotoKey) {
+        url += `/photos/${selectedPhotoKey}`;
+    }
+
+    if (location || year) {
+        url += '/?';
+        url += Object.entries({location, year})
+            .filter(e => e[1])
+            .map(entry => `${entry[0]}=${entry[1]}`)
+            .join('&');
+    }
+
+    return url;
+}
+
 export default (data, {selectedPhotoKey, year, location}) => {
     const match = data.photoData.photos.filter(p => p.key === selectedPhotoKey)[0];
+
+    let filterTitle;
+    if (location || year) {
+        filterTitle = [location, year]
+            .filter(e => e)
+            .map(str => (
+                str.charAt(0).toUpperCase() + str.slice(1)
+            ))
+            .join(' ');
+    }
 
     if (match) {
         const selectedPhoto = serverToClient(match, data.photoData.base);
@@ -13,8 +41,8 @@ export default (data, {selectedPhotoKey, year, location}) => {
         return {
             'og:type': 'article',
             'og:site_name': name,
-            'og:title': `${selectedPhoto.title} :: ${name}`,
-            'og:url': `http://www.thesplendidnudibranch.pink/photos/${selectedPhoto.key}`,
+            'og:title': [filterTitle || selectedPhoto.title, name].filter(e => e).join(' :: '),
+            'og:url': buildUrl({selectedPhotoKey, year, location}),
             'og:description': selectedPhoto.description,
             'og:image': selectedPhotoSize.url,
             'og:image:width': selectedPhotoSize.width,
@@ -22,24 +50,11 @@ export default (data, {selectedPhotoKey, year, location}) => {
         };
     }
 
-    let url = 'http://www.thesplendidnudibranch.pink';
-    let title;
-    if (location || year) {
-        url += '/?';
-        title = [location, year]
-            .filter(e => e)
-            .map(str => (
-                str.charAt(0).toUpperCase() + str.slice(1)
-            ))
-            .join(' ') + ` :: ${name}`;
-    }
-
-    url += Object.entries({location, year}).filter(e => e[1]).map(entry => `${entry[0]}=${entry[1]}`).join('&');
-
     return {
-        'og:title': title || name,
+        'og:type': 'article',
         'og:site_name': name,
-        'og:url': url,
+        'og:title': [filterTitle, name].filter(e => e).join(' :: '),
+        'og:url': buildUrl({year, location}),
         'og:description': description,
         'og:image': 'http://www.thesplendidnudibranch.pink/static/images/logo.png',
         'og:image:width': 1300,
