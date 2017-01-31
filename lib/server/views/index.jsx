3879f51d0c6fa3d16c3daf51dc8a402b76c1ab6b
Kris-Mikael Krister <krismikael@protonmail.com>
2017-01-31 21:40:28 +0100

    Reduce heroku build time by packaging locally

---
+++ lib/server/views/index.jsx
@@ -0,0 +1,53 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _default = require('./layouts/default');
+
+var _default2 = _interopRequireDefault(_default);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Index = function Index(_ref) {
+    var data = _ref.data,
+        keywords = _ref.keywords,
+        selectedPhotoKey = _ref.selectedPhotoKey,
+        year = _ref.year,
+        location = _ref.location;
+    return _react2.default.createElement(
+        _default2.default,
+        {
+            data: data,
+            keywords: keywords,
+            year: year,
+            location: location,
+            selectedPhotoKey: selectedPhotoKey },
+        _react2.default.createElement(
+            'div',
+            null,
+            _react2.default.createElement(
+                'div',
+                { id: 'app' },
+                _react2.default.createElement(
+                    'div',
+                    { className: 'loading-placeholder' },
+                    _react2.default.createElement('div', { className: 'spinner' })
+                )
+            ),
+            _react2.default.createElement('script', { type: 'text/javascript', dangerouslySetInnerHTML: { __html: '\n                window.sn = {\n                    data: ' + JSON.stringify(data) + '\n                };\n            ' } }),
+            _react2.default.createElement('script', {
+                type: 'text/javascript',
+                src: '/static/scripts/bundle.js',
+                async: true,
+                charSet: 'utf-8' })
+        )
+    );
+};
+
+exports.default = Index;
