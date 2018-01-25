473d3f78f3e9cb545c48f9fe6e565722647ef7bb
Kris-Mikael Krister <krismikael@protonmail.com>
2018-01-25 23:26:07 +0100

    Improve swipe

--- client/src/photos/touch-event-handler.js
+++ client/src/photos/touch-event-handler.js
@@ -6,6 +6,7 @@ class TouchEventHandler extends Component {
   constructor() {
     super()
     this.xDown = null
+    this.yDown = null
     this.handleTouchStart = this.handleTouchStart.bind(this)
     this.handleTouchMove = this.handleTouchMove.bind(this)
   }
@@ -26,6 +27,7 @@ class TouchEventHandler extends Component {
 
   handleTouchStart(evt) {
     this.xDown = evt.touches[0].clientX
+    this.yDown = evt.touches[0].clientY
   }
 
   handleTouchMove(evt) {
@@ -36,15 +38,20 @@ class TouchEventHandler extends Component {
     const { onNext, onPrevious } = this.props
 
     const xUp = evt.touches[0].clientX
+    const yUp = evt.touches[0].clientY
 
     const xDiff = this.xDown - xUp
+    const yDiff = this.yDown - yUp
 
-    if (xDiff > 0) {
-      onPrevious()
-    } else {
-      onNext()
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      if (xDiff > 0) {
+        onPrevious()
+      } else {
+        onNext()
+      }
     }
     this.xDown = null
+    this.yDown = null
   }
 
   render() {
