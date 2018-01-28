a3a1117c55fb1b246745f79e94219b7650d94e12
Kris-Mikael Krister <krismikael@protonmail.com>
2018-01-28 15:09:03 +0100

    Include license block

--- client/src/deep-water.js
+++ client/src/deep-water.js
@@ -3,6 +3,27 @@ import { h } from 'preact'
 
 const DeepWater = ({ onClick }) => (
   <div id="deep-water">
+    <div id="license">
+      <p xmlnsDct="http://purl.org/dc/terms/" property="dct:title">
+        The Splendid Nudibranch
+      </p>
+      <p>
+        <a
+          xmlnsCc="http://creativecommons.org/ns#"
+          href="mailto:krismikael-insertathere-protonmail-insertdothere-com"
+          property="cc:attributionName"
+          rel="cc:attributionURL"
+        >
+          Kris Mikael Krister
+        </a>
+      </p>
+      <p>
+        <a rel="license" href="https://creativecommons.org/licenses/by/4.0/">
+          CC BY 4.0
+        </a>
+      </p>
+    </div>
+
     <div class="link-wrapper">
       <a href="#" onClick={onClick}>
         DROP YOUR WEIGHTS
