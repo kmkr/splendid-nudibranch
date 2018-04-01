e84d9863a7e1a722a393f130c14d261a834fb3a9
Kris-Mikael Krister <krismikael@protonmail.com>
2018-04-01 22:07:32 +0200

    Add option to hide text in admin view

--- client/src/admin/photos/list-photos.js
+++ client/src/admin/photos/list-photos.js
@@ -1,40 +1,61 @@
 /** @jsx h */
-import { h } from 'preact'
+import { h, Component } from 'preact'
 
-const ListPhotos = ({ photos, onDeleteClick }) => (
-  <div className="list-photos">
-    {photos.map(photo => (
-      <div key={photo.key}>
-        <div className="center">
-          <h3>
-            {photo.name}
-            <button
-              className={`danger ${photo.deleting ? 'active' : ''}`}
-              disabled={photo.deleting}
-              onClick={onDeleteClick.bind(this, photo)}
-            >
-              ✖
-            </button>
-          </h3>
-          <img src={photo.sizes.thumb.url} />
-        </div>
-        <p>{photo.title}</p>
-        <p>{photo.location}</p>
-        <p>{photo.latin}</p>
-        <p>{photo.description}</p>
+class ListPhotos extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showText: true
+    }
 
-        <div className="tags">
-          {photo.tags.sort().map(tag => <span key={tag}>{tag}</span>)}
-        </div>
+    this.toggleText = this.toggleText.bind(this)
+  }
 
-        <div>
-          {photo.error && (
-            <p style={{ color: 'red' }}>Something bad happened!</p>
-          )}
-        </div>
+  toggleText() {
+    this.setState(oldState => ({
+      showText: !oldState.showText
+    }))
+  }
+
+  render({ photos, onDeleteClick }, { showText }) {
+    return (
+      <div className="list-photos">
+        <label>
+          Show text
+          <input type="checkbox" checked={showText} onClick={this.toggleText} />
+        </label>
+        {photos.map(photo => (
+          <div key={photo.key}>
+            <div className="center">
+              <h3>
+                {photo.name}
+                <button
+                  className={`danger ${photo.deleting ? 'active' : ''}`}
+                  disabled={photo.deleting}
+                  onClick={onDeleteClick.bind(this, photo)}
+                >
+                  ✖
+                </button>
+              </h3>
+              <img src={photo.sizes.thumb.url} />
+            </div>
+            {showText && (
+              <div>
+                <p>{photo.title}</p>
+                <p>{photo.location}</p>
+                <p>{photo.latin}</p>
+                <p>{photo.description}</p>
+
+                <div className="tags">
+                  {photo.tags.sort().map(tag => <span key={tag}>{tag}</span>)}
+                </div>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-)
+    )
+  }
+}
 
 export default ListPhotos
