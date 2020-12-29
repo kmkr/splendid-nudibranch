ce3f6b62627a0dd674c5d3541b2d1a3ba1f9b64c
Kris-Mikael Krister <kmk@otovo.com>
2020-12-29 23:30:50 +0100

    Re-do top part

--- src/contact-info.js
+++ src/contact-info.js
@@ -1,16 +1,8 @@
 const ContactInfo = () => (
   <div id="contact-info">
-    <p xmlnsdct="http://purl.org/dc/terms/" property="dct:title">
-      The Splendid Nudibranch
-    </p>
     <p>
       by{" "}
-      <a
-        xmlnscc="http://creativecommons.org/ns#"
-        href="mailto:krismikael-insertathere-protonmail-insertdothere-com"
-        property="cc:attributionName"
-        rel="cc:attributionURL"
-      >
+      <a href="mailto:krismikael-insertathere-protonmail-insertdothere-com">
         Kris Mikael Krister
       </a>
     </p>
--- src/top-logo.js
+++ src/top-logo.js
@@ -3,7 +3,14 @@ import ContactInfo from "./contact-info";
 
 const TopLogo = () => (
   <div id="top-logo">
-    <img src="/images/logo.svg" />
+    <div class="haiku-wrapper">
+      <img src="/images/logo_plain.svg" />
+      <div class="haiku">
+        <p>Mostly animals</p>
+        <p>Plus the occasional tree</p>
+        <p>The slug welcomes you</p>
+      </div>
+    </div>
     <ContactInfo />
   </div>
 );
